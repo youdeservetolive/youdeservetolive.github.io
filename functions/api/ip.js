556644d19f6fc8802f92374f42334a241ba8d38d
youdeservetolive <es+ydtl@prodbyabnormal.com>
2026-03-31 19:40:39 -0400

    Update ip.js

--- functions/api/ip.js
+++ functions/api/ip.js
@@ -1,3 +1,67 @@
+const COUNTRY_MAP = {
+  US: "United States",
+  DZ: "Algeria",
+  AO: "Angola",
+  AR: "Argentina",
+  AM: "Armenia",
+  AU: "Australia",
+  AT: "Austria",
+  BS: "Bahamas",
+  BH: "Bahrain",
+  BD: "Bangladesh",
+  BB: "Barbados",
+  BE: "Belgium",
+  BO: "Bolivia",
+  BA: "Bosnia & Herzegovina",
+  BW: "Botswana",
+  BR: "Brazil",
+  BG: "Bulgaria",
+  BF: "Burkina Faso",
+  BI: "Burundi",
+  CA: "Canada",
+  CN: "China",
+  CO: "Colombia",
+  CR: "Costa Rica",
+  HR: "Croatia",
+  CZ: "Czech Republic",
+  DK: "Denmark",
+  DO: "Dominican Republic",
+  EC: "Ecuador",
+  EG: "Egypt",
+  SV: "El Salvador",
+  EE: "Estonia",
+  ET: "Ethiopia",
+  FI: "Finland",
+  FR: "France",
+  DE: "Germany",
+  GH: "Ghana",
+  GR: "Greece",
+  IN: "India",
+  IT: "Italy",
+  JP: "Japan",
+  KE: "Kenya",
+  MY: "Malaysia",
+  MX: "Mexico",
+  NZ: "New Zealand",
+  KR: "Republic of Korea",
+  RU: "Russia",
+  SG: "Singapore",
+  ZA: "South Africa",
+  ES: "Spain",
+  GB: "United Kingdom",
+  ZW: "Zimbabwe"
+};
+
+const CONTINENT_MAP = {
+  AF: "Africa",
+  AN: "Antarctica",
+  AS: "Asia",
+  EU: "Europe",
+  NA: "North America",
+  OC: "Oceania",
+  SA: "South America"
+};
+
 export async function onRequest(context) {
   const { request } = context;
 
@@ -31,20 +95,44 @@ export async function onRequest(context) {
 
   try {
     const cf = request.cf || {};
+    const countryCode = cf.country || "";
+    const continentCode = cf.continent || "";
+
+    const countryName = COUNTRY_MAP[countryCode] || countryCode || "";
+    const continentName = CONTINENT_MAP[continentCode] || continentCode || "";
 
     return new Response(
       JSON.stringify({
-        success: true,
         ip: request.headers.get("CF-Connecting-IP") || "",
-        country: cf.country || "",
+        success: true,
+        type: "",
+        continent: continentName,
+        continent_code: continentCode,
+        country: countryName,
+        country_code: countryCode,
+        country_flag: countryCode
+          ? `https://cdn.ipwhois.io/flags/${countryCode.toLowerCase()}.svg`
+          : "",
+        country_capital: "",
+        country_phone: "",
+        country_neighbours: "",
         region: cf.region || "",
-        regionCode: cf.regionCode || "",
         city: cf.city || "",
-        continent: cf.continent || "",
-        postalCode: cf.postalCode || "",
-        latitude: cf.latitude || "",
-        longitude: cf.longitude || "",
-        timezone: cf.timezone || ""
+        latitude: cf.latitude ? Number(cf.latitude) : null,
+        longitude: cf.longitude ? Number(cf.longitude) : null,
+        asn: "",
+        org: "",
+        isp: "",
+        timezone: cf.timezone || "",
+        timezone_name: "",
+        timezone_dstOffset: null,
+        timezone_gmtOffset: null,
+        timezone_gmt: "",
+        currency: "",
+        currency_code: "",
+        currency_symbol: "",
+        currency_rates: null,
+        currency_plural: ""
       }),
       {
         status: 200,
